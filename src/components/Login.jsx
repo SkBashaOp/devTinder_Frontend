ebbf896b29ba298facb81800d64ed79869d1f728
Shaik Mohammed Hussain Basha <73814469+SkBashaOp@users.noreply.github.com>
2026-02-01 18:55:38 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { addUser } from "../store/userSlice";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+axios.defaults.baseURL = "";
 
 const Login = () => {
   const [firstName, setFirstName] = useState("");
