09455c7eaebdf270649d15138a0a6b8e2862cec2
Sameer <73814469+SkBashaOp@users.noreply.github.com>
2026-02-01 10:48:46 +0530

    Refactor connection fetching and improve error handling

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useEffect } from "react";
-import { BASE_URL } from "../utils/constant";
 import { useDispatch, useSelector } from "react-redux";
 import { addConnection } from "../store/connectionSlice";
 import Shimmer from "./Shimmer";
@@ -10,10 +9,16 @@ const Connections = () => {
   const connections = useSelector((store) => store.connection);
 
   const fetchConnections = async () => {
-    const res = await axios.get(BASE_URL + "user/request/accepted", {
-      withCredentials: true,
-    });
-    dispatch(addConnection(res?.data?.data));
+    try {
+      const res = await axios.get(
+        "/api/user/request/accepted",
+        { withCredentials: true }
+      );
+
+      dispatch(addConnection(res?.data?.data));
+    } catch (error) {
+      console.error("Failed to fetch connections", error);
+    }
   };
 
   useEffect(() => {
@@ -28,36 +33,41 @@ const Connections = () => {
     );
   }
 
-  if (connections.length === 0)
+  if (connections.length === 0) {
     return (
       <div className="w-full h-screen flex items-center justify-center">
         <h1>No connection yet!!</h1>
       </div>
     );
+  }
 
   return (
     <div className="w-full min-h-screen pt-[8vh] pb-[4vh] px-[4vw] flex flex-col items-center gap-[2vh] max-lg:h-fit">
       {connections.map((connection) => (
         <div className="w-[25vw] max-lg:w-full" key={connection._id}>
           <div className="flex items-center bg-base-100 shadow-xl pl-[1vw] w-fit">
-            <figure className="">
+            <figure>
               <img
                 src={connection.photoUrl}
-                alt="Movie"
+                alt="User"
                 className="max-lg:w-[12vw] max-lg:h-[12vw] w-[4vw] h-[4vw] rounded-full"
               />
             </figure>
+
             <div className="card-body">
               <h2 className="card-title">
                 {connection.firstName} {connection.lastName}
               </h2>
+
               <p>
                 {connection.age} {connection.gender}
               </p>
-              <p className="">{connection.about}</p>
+
+              <p>{connection.about}</p>
+
               <div className="card-actions justify-start">
                 <button className="bg-gray-500 text-white px-[1vw] py-[.8vh] rounded-md">
-                  remove
+                  Remove
                 </button>
               </div>
             </div>
