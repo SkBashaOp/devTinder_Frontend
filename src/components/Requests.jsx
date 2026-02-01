c20329678c9e49ec63f359ae8eafe9374db5faa1
Shaik Mohammed Hussain Basha <73814469+SkBashaOp@users.noreply.github.com>
2026-02-01 20:04:06 +0530

    Update Requests.jsx

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,4 +1,4 @@
-import api from "../utils/apiInstance";
+import axios from "axios";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addRequest, removRequestOnAccept } from "../store/requestSlice";
@@ -10,7 +10,7 @@ const Requests = () => {
 
   const fetchRequests = async () => {
     try {
-      const res = await api.get(
+      const res = await axios.get(
         "/api/user/request/received",
         { withCredentials: true }
       );
@@ -23,7 +23,7 @@ const Requests = () => {
 
   const handleAcceptRequest = async (status, id) => {
     try {
-      await api.post(
+      await axios.post(
         `/api/request/review/${status}/${id}`,
         {},
         { withCredentials: true }
